3a0a31c0b6543d1f972e707817b87057ee3691e1
Lazarski, Michael (OTD) <michael.lazarski@tchibo.de>
2021-01-02 17:25:06 +0100

    update new command and npm script to run eslint in parallel in every package

--- scripts/templates/packageJson.ts
+++ scripts/templates/packageJson.ts
@@ -4,11 +4,15 @@ export default ({ packageName }: { packageName: string }): string => {
   "version": "0.0.0",
   "main": "dist/index.js",
   "license": "MIT",
+  "scripts": {
+    "lint": "npx eslint -f codeframe --config='../../.eslintrc.js' --ext ts,vue ./"
+  },
   "peerDependencies": {
     "vue": "^3.0.4"
   },
   "devDependencies": {
-    "@vue/test-utils": "^2.0.0-beta.13"
+    "@vue/test-utils": "^2.0.0-beta.13",
+    "eslint": "^7.17.0"
   }
 }`;
 };
