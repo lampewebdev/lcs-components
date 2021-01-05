ec19a96101a0c4fdda168a7fd7983912d153c7de
Lazarski, Michael (OTD) <michael.lazarski@tchibo.de>
2021-01-05 21:09:34 +0100

    cleanup typography

--- packages/heading/src/index.vue
+++ packages/heading/src/index.vue
@@ -16,7 +16,41 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.lcs-Heading {
-  /* your style */
+h1 {
+  font-weight: 400;
+  font-style: normal;
+  font-size: 50px;
+  line-height: 55px;
+}
+h2 {
+  font-family: "Poppins", sans-serif;
+  font-weight: 200;
+  font-size: 2.5rem;
+}
+
+h3 {
+  font-family: "Poppins", sans-serif;
+  font-weight: 100;
+  font-size: 2.2rem;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  text-decoration: var(--act-color) underline;
+  font-family: "Comfortaa", sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  color: var(--text-color);
 }
 </style>
